25434235a4338aa527ae8a1de38fb45f43b0001c
Artem_Volodin <artemvolodin900000@gmail.com>
2021-09-22 17:17:36 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output:{
+        path: path.resolve(__dirname + '/public'),
+        filename: "bundle.js"
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 8081,
+        open: true
+    },
+    module:{
+        rules:[
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options:{
+                    presets:["@babel/preset-env", "@babel/preset-react"]
+                }
+            }
+        ]
+    }
+};
