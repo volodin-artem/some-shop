156eef81df6aac384b6946d21640ff59d9012e78
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-10 18:36:50 +0300

    Pages folder created

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Main from "./pages/Main.jsx";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Main />
+
       </div>
     );
   }
