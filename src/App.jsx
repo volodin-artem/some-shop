736e978bd99e38c3aede8f15980673c32a1decb3
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-10 18:29:40 +0300

    Folders renamed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Info from './header/Info.jsx';
 import Header from './header/Header.jsx';
-import NavMenu from "./navMenu/NavMenu.jsx";
+import NavMenu from "./nav-menu/NavMenu.jsx";
 import Banner from "./banner/Banner.jsx";
-import PopularProductContainer from "./popularProduct/PopularProductContainer.jsx";
+import PopularProductContainer from "./popular-product/PopularProductContainer.jsx";
 import RecentlyViewed from "./recently-viewed/RecentlyViewed.jsx";
 import Footer from "./footer/Footer.jsx";
 
