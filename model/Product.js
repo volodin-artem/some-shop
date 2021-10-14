aaf4d341ff12478edaa9dcd97eee7ad959e2721c
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-14 15:09:12 +0300

    Product model is done

---
+++ model/Product.js
@@ -0,0 +1,33 @@
+const {Sequelize} = require('sequelize');
+const sequelize = require('../app.js').sequelize;
+
+const Product = sequelize.define("Product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  imagePath: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  viewsCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
