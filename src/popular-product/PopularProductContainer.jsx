01a32ac49b5a275b3e873b0823fdacc49a4c4e94
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-14 15:10:06 +0300

    Popular products fetch added

--- src/popular-product/PopularProductContainer.jsx
+++ src/popular-product/PopularProductContainer.jsx
@@ -5,19 +5,28 @@ import Product from "../product/Product.jsx";
 class PopularProductContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {items: []};
+    this.getProducts();
+  }
+
+  getProducts(){
+    return fetch("http://localhost:3000/popular-products", {
+      method: "GET"
+    }).then( res => res.json()).then( (res) => {
+      let arr = [];
+      for (let item of res) {
+        arr.push( <Product price={item.price} desc={item.name} imgSrc={item.imagePath} /> );
+      }
+      this.setState({items: arr});
+    });
   }
 
   render() {
-    return (
-      <div>
+      return(
+        <div>
         <p className="container-label content">Популярные товары</p>
         <div className="content popular-product-container">
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
-          <Product imgSrc="https://ru.reactjs.org/logo-og.png" price="200000" desc="React" />
+          {this.state.items}
         </div>
       </div>
     );
