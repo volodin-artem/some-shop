c33b9418325e30529ec1f57ff2edbbdf84588704
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-15 21:16:32 +0300

    Subcategories model is done

---
+++ model/Subcategories.js
@@ -0,0 +1,17 @@
+const {Sequelize} = require('sequelize');
+const sequelize = require('../app.js').sequelize;
+const ProductTypes = require('./ProductTypes.js');
+
+const Subcategories = sequelize.define("Subcategories", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+module.exports = Subcategories;
