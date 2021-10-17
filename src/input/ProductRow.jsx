1449537ead517c553b4f615ea982d394078f2c89
Artem_Volodin <artemvolodin900000@gmail.com>
2021-10-17 16:27:51 +0300

    Rating with stars is started

--- src/input/ProductRow.jsx
+++ src/input/ProductRow.jsx
@@ -24,6 +24,15 @@ class ProductRow extends React.Component {
               Купить
             </div>
           </div>
+          <div className="product__bottom">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="black">
+              <linearGradient id="linear-gradient">
+                <stop offset="50%" stop-color="#D39200"/>
+                <stop offset="50%" stop-color="white"/>
+              </linearGradient>
+              <path fill="url(#linear-gradient)" d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/>
+            </svg>
+          </div>
         </div>
       </a>
     );
