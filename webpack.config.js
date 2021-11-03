4d7e00fe65d1775c6bf548ad7844499477194e9f
Artem_Volodin <artemvolodin900000@gmail.com>
2021-11-03 14:37:18 +0300

    Webpack config indents changed to 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
 const path = require('path');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname + '/public'),
-        filename: "bundle.js"
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 1337,
-        open: true,
-        host: 'localhost'
-    },
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /(node_modules)/,
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                        plugins: ["@babel/plugin-transform-runtime"]
-                    }
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        "sass-loader"
-                    ]
-                }
-            ]
+  devtool: 'eval-source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname + '/public'),
+    filename: "bundle.js"
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 1337,
+    open: true,
+    host: 'localhost'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-transform-runtime"]
         }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
 };
